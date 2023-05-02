4bd72d995605b9abc9cdac59dc61f311c943971b
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-05-02 23:20:36 +0500

    changed

--- dz5/task5.py
+++ dz5/task5.py
@@ -5,9 +5,9 @@ from random import randint
 def way(x, y):
     #print(f'{x} ---- {y}')
     if x == 4 and y == 4:
-        #print('ура!!!! есть путь!!!')
-        return 'yes'        
-    if x < 4 or y < 4:
+        print('ура!!!! есть путь!!!')
+        return 'yes'
+    elif x < 4 or y < 4:
         if y < 4 and field[x][y + 1] == 1:
             #print(f'пошли по X {x} : {y + 1} -> {field[x][y + 1]}')
             way(x, y + 1)
@@ -20,16 +20,20 @@ def way(x, y):
             # if x > 1 and field[x - 1][y] == 1:
             #     print(f'пошли по Y {x - 1} : {y} -> {field[x - 1][y]}')
             #     way(x - 1, y)     
-        else:
-            #print('похоже нет пути((((')
-            return 'no'
+        # elif y < 4 and field[x][y + 1] == 0:
+        #     print('Похоже нет пути!1')
+        # elif x < 4 and field[x + 1][y] == 0:
+        #     print('Похоже нет пути!2')
+        # else:
+        #     print('похоже нет пути((((')
+        #     return 'no'
     else:
-        #print('похоже нет пути((((')
+        print('похоже нет пути((((')
         return 'no'
 field = [ [1, 1, 1, 1, 1],
           [1, 0, 0, 0, 1], 
-          [1, 1, 1, 1, 1],
-          [1, 0, 0, 0, 0],
+          [1, 0, 1, 1, 0],
+          [1, 0, 0, 0, 1],
           [1, 1, 1, 1, 1] ]
 # field = [ ['', '', '', '', ''],
 #           ['', '', '', '', ''],
@@ -48,10 +52,17 @@ field = [ [1, 1, 1, 1, 1],
 # field[4][4] = 1
 print('', field[0],'\n', field[1], '\n', field[2], '\n', field[3], '\n', field[4])
 
-if way(0, 0) == 'yes':
-    print('Путь есть!')
-elif way(0, 0) == 'no':
-    print('Нет пути!')
+#w = way(0, 0)
+print(way(0, 0))
+# if w == 'yes':
+#     print('Путь есть!')
+# else:
+#     print('Нет пути!')
+
+# if way(0, 0) == 'yes':
+#     print('Путь есть!')
+# elif way(0, 0) == 'no':
+#     print('Нет пути!')
 
 #print(way(0, 0))
 #a = way(0, 0)
